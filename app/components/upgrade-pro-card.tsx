26f00d219dcb6add0870c0af45e668acc86333c7
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2025-09-03 17:14:55 +0700

    fix: update pricing display in upgrade pro card component

--- app/components/upgrade-pro-card.tsx
+++ app/components/upgrade-pro-card.tsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router"
 import { Button } from "./ui/button"
-import { toIDR } from "~/lib/utils"
 
 export default () => {
     return (
         <div className="flex flex-col justify-between bg-primary/90 p-4 rounded-2xl h-55 promotion">
-            <div className="promote-text">
+            <div>
                 <h1 className="font-bold uppercase">Upgrade to Pro</h1>
                 <h2 className="font-semibold text-2xl">
-                    <span className="text-black/50 line-through">{toIDR(900000)}</span> <span>{toIDR(750000)}</span> /mo
+                    <span className="text-black/50 line-through">Rp. 750K</span> <span>Rp. 100K</span> /mo
                 </h2>
             </div>
-            <p className="mb-4">
+            <p className="mb-4 w-8/12">
                 Get access to all premium features and content
             </p>
             <div className="action">
