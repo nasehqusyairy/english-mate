1c4c57e2f83c584617b08affea878f6a5710dce7
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2025-09-05 20:09:48 +0700

    feat: add PWA support with vite-plugin-pwa and update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: false, // pakai manifest.json dari public/
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+  }),
+  ],
   server: {
     host: '0.0.0.0'
   }
